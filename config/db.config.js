b9acc02f946141a60e1f6be0e95c67e82f026dc4
adifdwimaulana <adifdwimaulana@gmail.com>
2021-04-07 13:35:43 +0700

    update db credential heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 const config = {
-    url: "mongodb://localhost:27017/text-checker?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
-    // url: "mongodb://textchecker:textchecker@checkerdb-shard-00-00.eie7n.mongodb.net:27017,checkerdb-shard-00-01.eie7n.mongodb.net:27017,checkerdb-shard-00-02.eie7n.mongodb.net:27017/text-checker?authSource=admin&replicaSet=atlas-13lshf-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+    // url: "mongodb://localhost:27017/text-checker?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
+    url: "mongodb://textchecker:textchecker@checkerdb-shard-00-00.eie7n.mongodb.net:27017,checkerdb-shard-00-01.eie7n.mongodb.net:27017,checkerdb-shard-00-02.eie7n.mongodb.net:27017/text-checker?authSource=admin&replicaSet=atlas-13lshf-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
     options: {
         useNewUrlParser: true,
         useCreateIndex: true,
